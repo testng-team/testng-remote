87cd2f47b93d442acbb43ae885096b1b9b84d0ee
Nick Tan <missedone@gmail.com>
2016-07-30 20:24:48 -0700

    fix issue #29: scan the jar, custom parsing service provider file to bypass the classloading issue on INDEX.LIST

--- remote/src/main/java/org/testng/remote/support/ServiceLoaderHelper.java
+++ remote/src/main/java/org/testng/remote/support/ServiceLoaderHelper.java
@@ -1,11 +1,18 @@
 package org.testng.remote.support;
 
-import org.osgi.framework.Version;
-import org.testng.TestNGException;
-
+import java.io.*;
+import java.net.URL;
+import java.net.URLClassLoader;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.ServiceConfigurationError;
 import java.util.ServiceLoader;
+import java.util.jar.JarEntry;
+import java.util.jar.JarFile;
+
+import org.osgi.framework.Version;
+import org.testng.TestNGException;
+import org.testng.remote.RemoteTestNG;
 
 public final class ServiceLoaderHelper {
 
@@ -26,4 +33,104 @@ public final class ServiceLoaderHelper {
         }
         return factories.get(0);
     }
+
+  public static RemoteTestNGFactory getFirstQuietly(String version) {
+    return getFirstQuietly(Version.parseVersion(version));
+  }
+
+  /**
+   * Get the first RemoteTestNGFactory on classpath.
+   * <p>
+   * this implementation is diff with {@link #getFirst(Version)} that 
+   * it scans the JARs on the classpath, and parse the services file manually.
+   * </p>
+   * @param version
+   * @return
+   * @throws TestNGException if not found
+   */
+  public static RemoteTestNGFactory getFirstQuietly(Version version) {
+    ClassLoader cl = RemoteTestNGFactory.class.getClassLoader();
+    if (cl instanceof URLClassLoader) {
+      for (URL url : ((URLClassLoader) cl).getURLs()) {
+        File f = new File(url.getFile());
+        // only check for jar file name starts with 'testng-remote'
+        if (f.isFile() && f.getName().startsWith("testng-remote")) {
+          try (JarFile jarFile = new JarFile(f)) {
+            JarEntry entry = jarFile.getJarEntry("META-INF/services/" + RemoteTestNGFactory.class.getName());
+            if (entry != null) {
+              ArrayList<String> names = new ArrayList<>();
+
+              try (BufferedReader r = new BufferedReader(
+                  new InputStreamReader(jarFile.getInputStream(entry), "utf-8"))) {
+                int lc = 1;
+                while ((lc = parseLine(RemoteTestNGFactory.class, url, r, lc, names)) >= 0) {
+                  // noop
+                }
+
+                for (String name : names) {
+                  Class<?> c = Class.forName(name, false, cl);
+                  RemoteTestNGFactory factory = (RemoteTestNGFactory) c.newInstance();
+                  if (factory.accept(version)) {
+                    return factory;
+                  }
+                }
+              }
+            }
+          } catch (TestNGException ex) {
+            throw ex;
+          } catch (Exception ex) {
+            if (RemoteTestNG.isDebug()) {
+              ex.printStackTrace();
+            }
+          }
+        }
+      }
+    }
+
+    throw new TestNGException(version + " is not a supported TestNG version");
+  }
+
+  // Parse a single line from the given configuration file, adding the name
+  // on the line to the names list.
+  //
+  private static int parseLine(Class<?> service, URL u, BufferedReader r, int lc, List<String> names)
+      throws IOException, ServiceConfigurationError {
+    String ln = r.readLine();
+    if (ln == null) {
+      return -1;
+    }
+    int ci = ln.indexOf('#');
+    if (ci >= 0) {
+      ln = ln.substring(0, ci);
+    }
+    ln = ln.trim();
+    int n = ln.length();
+    if (n != 0) {
+      if ((ln.indexOf(' ') >= 0) || (ln.indexOf('\t') >= 0)) {
+        fail(service, u, lc, "Illegal configuration-file syntax");
+      }
+      int cp = ln.codePointAt(0);
+      if (!Character.isJavaIdentifierStart(cp)) {
+        fail(service, u, lc, "Illegal provider-class name: " + ln);
+      }
+      for (int i = Character.charCount(cp); i < n; i += Character.charCount(cp)) {
+        cp = ln.codePointAt(i);
+        if (!Character.isJavaIdentifierPart(cp) && (cp != '.')) {
+          fail(service, u, lc, "Illegal provider-class name: " + ln);
+        }
+      }
+      if (!names.contains(ln)) {
+        names.add(ln);
+      }
+    }
+    return lc + 1;
+  }
+
+  private static void fail(Class<?> service, String msg) throws ServiceConfigurationError {
+    throw new ServiceConfigurationError(service.getName() + ": " + msg);
+  }
+
+  private static void fail(Class<?> service, URL u, int line, String msg) throws ServiceConfigurationError {
+    fail(service, u + ":" + line + ": " + msg);
+  }
 }
