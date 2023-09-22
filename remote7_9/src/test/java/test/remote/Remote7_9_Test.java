2ec46c36cf16ddb251d065679beb8263809d8834
Nick Tan <missedone@gmail.com>
2023-09-22 15:49:34 -0700

    feat: support testng 7.9.0

---
+++ remote7_9/src/test/java/test/remote/Remote7_9_Test.java
@@ -0,0 +1,12 @@
+package test.remote;
+
+import org.testng.internal.Version;
+
+public class Remote7_9_Test extends RemoteTest {
+
+  @Override
+  protected String getTestNGVersion() {
+    return Version.VERSION;
+  }
+
+}
